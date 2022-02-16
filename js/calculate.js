fab2d701cc820306bf669ec29c25b226a5647cb2
Murad Sarker <sarker24@gmail.com>
2022-02-16 23:32:51 +0100

    fix income and balance

--- js/calculate.js
+++ js/calculate.js
@@ -1,1 +1,22 @@
-console.log("sheow the calculate file");
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const totalExpenses = calculateExpense();
+  document.getElementById("calculate-expense").innerText = totalExpenses;
+  // calculateExpense.innerText = totalExpenses;
+  const totalIncome = document.getElementById("income-field").value;
+  const remainingBalance = balance(totalIncome, totalExpenses);
+  console.log(remainingBalance);
+});
+
+function calculateExpense() {
+  const foodText = document.getElementById("food-field").value;
+  const rentText = document.getElementById("rent-field").value;
+  const othersText = document.getElementById("others-field").value;
+  const totalExpense =
+    parseFloat(foodText) + parseFloat(rentText) + parseFloat(othersText);
+  return totalExpense;
+}
+
+function balance(income, expense) {
+  const remaining = income - expense;
+  return remaining;
+}
