6669561373f534550675a1a2b6523abd3bfde433
Murad Sarker <sarker24@gmail.com>
2022-02-17 01:02:09 +0100

    Input validation check

--- js/calculate.js
+++ js/calculate.js
@@ -3,8 +3,18 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   document.getElementById("calculate-expense").innerText = totalExpenses;
   // calculateExpense.innerText = totalExpenses;
   const totalIncome = document.getElementById("income-field").value;
-  const remainingBalance = balance(totalIncome, totalExpenses);
-  console.log(remainingBalance);
+  const calculateBalance = balance(totalIncome, totalExpenses);
+  //console.log(remainingBalance);
+  document.getElementById("calculate-balance").innerText = calculateBalance;
+});
+
+document.getElementById("save-btn").addEventListener("click", function () {
+  const totalIncome = document.getElementById("income-field").value;
+  const savingPercent = document.getElementById("saving-field").value;
+  const calculateSaving = savings(totalIncome, savingPercent);
+  console.log(calculateSaving);
+  document.getElementById("saving-amount").innerText = calculateSaving;
+  remainingBalance();
 });
 
 function calculateExpense() {
@@ -16,7 +26,29 @@ function calculateExpense() {
   return totalExpense;
 }
 
+function remainingBalance() {
+  const balanceText = document.getElementById("calculate-balance").innerText;
+  const savingText = document.getElementById("saving-amount").innerText;
+  console.log(balanceText, savingText);
+  document.getElementById("remaining-field").innerText =
+    parseFloat(balanceText) - parseFloat(savingText);
+}
+
 function balance(income, expense) {
   const remaining = income - expense;
   return remaining;
+}
+
+function savings(amount, percent) {
+  const savings = (amount * percent) / 100;
+  return savings;
+}
+
+function isNumber(evt) {
+  evt = evt ? evt : window.event;
+  var charCode = evt.which ? evt.which : evt.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false;
+  }
+  return true;
 }
